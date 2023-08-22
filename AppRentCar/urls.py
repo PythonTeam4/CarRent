035d392864119aabccd69d5c608d2e1d3373bb7d
Foxety <matt.lis.1998@gmail.com>
2023-08-22 15:20:45 +0200

    Add CreateView, DeleteView and UpdateView and change Pillow version to 9.5.0

--- AppRentCar/urls.py
+++ AppRentCar/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import CarListView, CarDetailView
+from .views import CarListView, CarDetailView, CarCreateView, CarUpdateView, \
+    CarDeleteView
 
 urlpatterns = [
     path('cars/', CarListView.as_view(), name="cars"),
-    path('cars/<int:pk>/', CarDetailView.as_view(), name='cars_detail'),
-
-
+    path('cars/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+    path('cars/create/', CarCreateView.as_view(), name='cars_create'),
+    path('cars/<int:pk>/update/', CarUpdateView.as_view(), name='cars_update'),
+    path('cars/<int:pk>/delete/', CarDeleteView.as_view(), name='cars_delete')
 ]
--- AppRentCar/views.py
+++ AppRentCar/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, FormView, UpdateView, \
+    DeleteView
 
+from .forms import CarForm
 from .models import Car
 
 
@@ -14,4 +17,31 @@ class CarListView(ListView):
 class CarDetailView(DetailView):
     template_name = "car.html"
     model = Car
+
+
+class CarCreateView(FormView):
+    template_name = 'form.html'
+    form_class = CarForm
+    success_url = '/cars/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class CarUpdateView(UpdateView):
+    template_name = 'form.html'
+    model = Car
+    fields = '__all__'
+
+    def get_success_url(self):
+        car_id = self.kwargs['pk']
+        return reverse_lazy('car_detail', args=[str(car_id)])
+
+
+class CarDeleteView(DeleteView):
+    template_name = 'confirm_delete.html'
+    model = Car
+    success_url = '/cars/'
+
 
