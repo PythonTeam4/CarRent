8f08841baa5c3c3b6783946e3cdb669bde94b1fb
ppieron <piotrek.pieron@gmail.com>
2023-09-02 14:35:28 +0200

    changed rent form formating

--- AppRentCar/forms.py
+++ AppRentCar/forms.py
@@ -15,30 +15,13 @@ class RentForm(forms.ModelForm):
     class Meta:
         model = Rent
         fields = ['rental_terms', 'start_date', 'end_date', 'take_from', 'take_back']
-
-    def __init__(self, *args, **kwargs):
-        car_id = kwargs.pop('car_id', None)
-        user_is_premium = kwargs.pop('user_is_premium', False)
-        super().__init__(*args, **kwargs)
-        self.user_is_premium = user_is_premium
-
-        if car_id:
-            rental_terms_queryset = RentalTerms.objects.filter(car_id=car_id)
-            if user_is_premium:
-                rental_terms_queryset = rental_terms_queryset.filter(premium_price__isnull=False)
-            else:
-                rental_terms_queryset = rental_terms_queryset.filter(premium_price__isnull=True)
-            self.fields['rental_terms'].queryset = rental_terms_queryset
-
-    def get_rental_terms_choices(self):
-        choices = []
-        for rental_terms in self.fields['rental_terms'].queryset:
-            if self.user_is_premium:
-                price_display = f"Premium: {rental_terms.premium_price}"
-            else:
-                price_display = f"Regular: {rental_terms.regular_price}"
-            choices.append((rental_terms.id, f"{rental_terms.car.brand} {rental_terms.car.model} - {price_display}"))
-        return choices
+        widgets = {
+            'rental_terms': forms.Select(attrs={'class': 'form-select'}),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'take_from': forms.Select(attrs={'class': 'form-select'}),
+            'take_back': forms.Select(attrs={'class': 'form-select'}),
+        }
 
 
 class SignUpForm(UserCreationForm):
