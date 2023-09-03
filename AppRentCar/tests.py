81ec483a4fa34f6668a402c6bd66a13e051c6f06
Foxety <matt.lis.1998@gmail.com>
2023-09-03 12:48:59 +0200

    Add test to CarUpdateView

--- AppRentCar/tests.py
+++ AppRentCar/tests.py
@@ -6,63 +6,6 @@ from django.utils import timezone
 from .models import Rent, RentalTerms, Car, CompanyBranches
 
 
-class RentAvailabilityTest(TestCase):
-
-    def setUp(self):
-        User = get_user_model()
-        self.user = User.objects.create_user(
-            username='testuser', password='testpassword')
-        self.car = Car.objects.create(
-            brand='Brand',
-            model='Model',
-            cars_type='Suv',
-            engine='Benzyna',
-            capacity=2.0,
-            year='2023',
-            number_of_seats=5,
-            consumption='8L/100km',
-            power='150KM',
-            car_mileage='10 000 km',
-            transmission='Automatyczna',
-            no_gears='6',
-            drive='Przedni'
-        )
-        self.rental_terms = RentalTerms.objects.create(
-            car=self.car, price=100.00)
-        self.start_date = timezone.now()
-        self.end_date = self.start_date + timezone.timedelta(days=5)
-
-    def test_car_available(self):
-        rent = Rent(rental_terms=self.rental_terms,
-                    start_date=self.start_date, end_date=self.end_date)
-        self.assertTrue(rent.is_car_available())
-
-    def test_car_unavailable(self):
-        company_branch = CompanyBranches.objects.create(city='Test City')
-        Rent.objects.create(
-            rental_terms=self.rental_terms,
-            client=self.user,
-            start_date=self.start_date,
-            end_date=self.end_date,
-            take_from=company_branch,
-            take_back=company_branch,
-        )
-        rent = Rent(
-            rental_terms=self.rental_terms,
-            client=self.user,
-            start_date=self.start_date,
-            end_date=self.end_date,
-            take_from=company_branch,
-            take_back=company_branch,
-        )
-        self.rental_terms = RentalTerms.objects.create(
-            car=self.car, price=100.00)
-        self.start_date = timezone.now()
-        self.end_date = self.start_date + timezone.timedelta(days=5)
-
-        self.assertFalse(rent.is_car_available())
-
-
 class RentCreateViewTest(TestCase):
     def setUp(self):
         User = get_user_model()
@@ -118,8 +61,7 @@ class RentCreateViewTest(TestCase):
 
         self.assertFalse(rent.is_car_available())
 
-    def test_form_validation(self):
-        # Nie ma potrzeby tworzenia osobnego klienta, użyj self.client, który jest dostępny w testach Django
+    def test_rental_creation_when_proper_data(self):
 
         self.client.login(username='testuser', password='testpass')
 
@@ -195,3 +137,47 @@ class RentCreateViewTest(TestCase):
         self.assertFalse(response.context['form'].is_valid())
         self.assertEqual(response.status_code, 200)
         self.assertTrue(Rent.objects.filter(rental_terms=self.rental_terms).exists())
+
+
+class CarUpdateViewTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.staff_user = User.objects.create_user(
+            username='staffuser',
+            password='testpassword',
+            is_staff=True
+        )
+
+        self.non_staff_user = User.objects.create_user(
+            username='ordinaryuser',
+            password='testpassword'
+        )
+        self.car = Car.objects.create(
+            brand='Brand',
+            model='Model',
+            cars_type='Suv',
+            engine='Benzyna',
+            capacity=2.0,
+            year='2023',
+            number_of_seats=5,
+            consumption='8L/100km',
+            power='150KM',
+            car_mileage='10 000 km',
+            transmission='Automatyczna',
+            no_gears='6',
+            drive='Przedni'
+        )
+
+    def test_car_update_view_access_for_staff(self):
+        self.client.login(username='staffuser', password='testpassword')
+
+        response = self.client.get(reverse('cars_update', args=[self.car.pk]))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_car_update_view_access_for_non_staff(self):
+        self.client.login(username='ordinaryuser', password='testpassword')
+
+        response = self.client.get(reverse('cars_update', args=[self.car.pk]))
+
+        self.assertEqual(response.status_code, 403)
